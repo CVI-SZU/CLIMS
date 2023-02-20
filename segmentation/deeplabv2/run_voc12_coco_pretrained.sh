a61cd04c6931e12733b857fbbec80e822df694bf
sierkinhane <sierkinhane@gmail.com>
2023-02-20 12:55:09 +0800

    update bash

--- segmentation/deeplabv2/run_voc12_coco_pretrained.sh
+++ segmentation/deeplabv2/run_voc12_coco_pretrained.sh
@@ -3,5 +3,5 @@ OMP_NUM_THREADS=32 CUDA_VISIBLE_DEVICES=6 python main.py test --config-path conf
 OMP_NUM_THREADS=32 CUDA_VISIBLE_DEVICES=6 python main.py crf --config-path configs/voc12_coco_pretrained.yaml
 OMP_NUM_THREADS=32 CUDA_VISIBLE_DEVICES=6 python eval.py --config_path configs/voc12_coco_pretrained.yaml --model_path data/models/voc12_coco_pretrained/deeplabv2_resnet101_msc/train_aug/checkpoint_final.pth
 # submit results.tar.gz to the evaluation server
-cd data/features/voc12_imagenet_pretrained/deeplabv2_resnet101_msc/test/
+cd data/features/voc12_coco_pretrained/deeplabv2_resnet101_msc/test/
 tar -czf results.tar.gz results
